4b21f50a4c76dc59cdf758f06c16eaf468308190
Kyrpychenko <denniskyrpychenko@gmail.com>
2024-01-05 09:37:50 +0100

    player and dealer can draw cards now

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,39 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { Card, Deck } from "../cards";
-const deck = ref<Deck | null>([]);
-const playerHand = ref<Deck | null>([]);
-const dealerHand = ref<Deck | null>([]);
+const deck = ref<Deck>([]);
+const playerHand = ref<Deck>([]);
+const dealerHand = ref<Deck>([]);
 function deckBuilder() {
   for (let i = 2; i < 11; i++) {
     let card: Card = {
       kind: "heart",
-      value: i,
-      name: i.toString(),
+      value: i as typeof card.value,
+      name: i.toString() as typeof card.name,
     };
     deck?.value.push(card);
   }
   for (let i = 2; i < 11; i++) {
     let card: Card = {
       kind: "club",
-      value: i,
-      name: i.toString(),
+      value: i as typeof card.value,
+      name: i.toString() as typeof card.name,
     };
     deck?.value.push(card);
   }
   for (let i = 2; i < 11; i++) {
     let card: Card = {
       kind: "diamond",
-      value: i,
-      name: i.toString(),
+      value: i as typeof card.value,
+      name: i.toString() as typeof card.name,
     };
     deck?.value.push(card);
   }
   for (let i = 2; i < 11; i++) {
     let card: Card = {
       kind: "spade",
-      value: i,
-      name: i.toString(),
+      value: i as typeof card.value,
+      name: i.toString() as typeof card.name,
     };
     deck?.value.push(card);
   }
@@ -58,32 +58,94 @@ function deckBuilder() {
   deck.value?.push({ kind: "spade", value: 10, name: "King" });
   deck.value?.push({ kind: "spade", value: 11, name: "Ace" });
 
-  // reduce function for shuffling the deck
-  // return deck.value.reduceRight(
-  //   (p, v, i, a) => (
-  //     (v = i ? ~~(Math.random() * (i + 1)) : i),
-  //     v - i ? ([deck.value[v], a[i]] = [deck.value[i], deck.value[v]]) : 0,
-  //     deck.value
-  //   ),
-  //   deck.value
-  // );
+  // shuffle the deck
+  return deck.value.reduceRight(
+    (p, v, i, a) => (
+      (v = i ? ~~(Math.random() * (i + 1)) : i),
+      v - i ? ([deck.value[v], a[i]] = [deck.value[i], deck.value[v]]) : 0,
+      deck.value
+    ),
+    deck.value
+  );
 }
 deckBuilder();
+
+function playerDrawCard() {
+  let specificCard = deck.value[Math.floor(Math.random() * deck.value.length)];
+  playerHand.value.push(specificCard);
+  deck.value.splice(deck.value.indexOf(specificCard), 1);
+}
+
+function dealerDrawCard() {
+  let specificCard = deck.value[Math.floor(Math.random() * deck.value.length)];
+  dealerHand.value.push(specificCard);
+  deck.value.splice(deck.value.indexOf(specificCard), 1);
+}
 </script>
 <template>
-  <div class="d-flex flex-wrap">
+  <!-- <div class="d-flex flex-wrap">
     <template v-for="card in deck">
       <div
         v-if="
-          (card.name != 'Jack' ||
-            card.name != 'Queen' ||
-            card.name != 'Ace' ||
-            card.name != 'King') &&
+          card.name != 'Jack' &&
+          card.name != 'Queen' &&
+          card.name != 'Ace' &&
+          card.name != 'King' &&
           card.value != 11
         "
       >
         <img
-          :src="'./cards/' + card.value + '_of_' + card.kind + 's.png'"
+          :src="'./cards/' + card.name + '_of_' + card.kind + 's.png'"
+          width="100"
+        />
+      </div>
+      <div
+        v-if="
+          card.name == 'Jack' ||
+          card.name == 'Queen' ||
+          card.name == 'Ace' ||
+          card.name == 'King'
+        "
+      >
+        <img
+          :src="
+            './cards/' + card.name.toLowerCase() + '_of_' + card.kind + 's2.png'
+          "
+          width="100"
+        />
+      </div>
+    </template>
+</div> -->
+  <button
+    class="btn btn-primary position-absolute"
+    style="right: 0"
+    @click="dealerDrawCard()"
+  >
+    Dealer Draw
+  </button>
+  <button
+    class="btn btn-primary position-absolute"
+    style="right: 0; top: 5%"
+    @click="playerDrawCard()"
+  >
+    Player Draw
+  </button>
+  <div class="text-white fs-1 position-absolute" style="top: 20%; left: 48%">
+    {{ dealerHand.reduce((a, b) => a + b.value, 0) }}
+  </div>
+  <div class="d-flex flex-wrap position-absolute" style="left: 45%">
+    <template v-for="card in dealerHand">
+      <div
+        v-if="
+          card.name != 'Jack' &&
+          card.name != 'Queen' &&
+          card.name != 'Ace' &&
+          card.name != 'King' &&
+          card.value != 11
+        "
+      >
+        <img
+          :src="'./cards/' + card.name + '_of_' + card.kind + 's.png'"
           width="100"
         />
       </div>
@@ -104,6 +166,45 @@ deckBuilder();
       </div>
     </template>
   </div>
-  {{ deck }}
+  <div
+    class="text-white fs-1 position-absolute"
+    style="bottom: 20%; left: 48%"
+    :class="{ 'text-danger': playerHand.reduce((a, b) => a + b.value, 0) > 21 }"
+  >
+    {{ playerHand.reduce((a, b) => a + b.value, 0) }}
+  </div>
+  <div class="d-flex flex-wrap position-absolute" style="left: 45%; bottom: 0">
+    <template v-for="card in playerHand">
+      <div
+        v-if="
+          card.name != 'Jack' &&
+          card.name != 'Queen' &&
+          card.name != 'Ace' &&
+          card.name != 'King' &&
+          card.value != 11
+        "
+      >
+        <img
+          :src="'./cards/' + card.name + '_of_' + card.kind + 's.png'"
+          width="100"
+        />
+      </div>
+      <div
+        v-if="
+          card.name == 'Jack' ||
+          card.name == 'Queen' ||
+          card.name == 'Ace' ||
+          card.name == 'King'
+        "
+      >
+        <img
+          :src="
+            './cards/' + card.name.toLowerCase() + '_of_' + card.kind + 's2.png'
+          "
+          width="100"
+        />
+      </div>
+    </template>
+  </div>
 </template>
 <style scoped lang="scss"></style>
